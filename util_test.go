1dc2cccccd823f69c4b7c76f88137fdc3857dbe3
Fabio Tranchitella <fabio.tranchitella@northern.tech>
2021-02-21 05:57:16 +0100

    Unit test for getRemoteURLGitLab
    Changelog: none
    
    Signed-off-by: Fabio Tranchitella <fabio.tranchitella@northern.tech>

--- util_test.go
+++ util_test.go
@@ -3,7 +3,7 @@ package main
 import (
 	"testing"
 
-	"gotest.tools/assert"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetRemoteURLGitHub(t *testing.T) {
@@ -12,4 +12,18 @@ func TestGetRemoteURLGitHub(t *testing.T) {
 
 	url = getRemoteURLGitHub(GitProtocolHTTP, "mendersoftware", "workflows")
 	assert.Equal(t, "https://github.com/mendersoftware/workflows", url)
+}
+
+func TestGetRemoteURLGitLab(t *testing.T) {
+	url, err := getRemoteURLGitLab("mendersoftware", "workflows")
+	assert.NoError(t, err)
+	assert.Equal(t, "git@gitlab.com:Northern.tech/Mender/workflows", url)
+
+	url, err = getRemoteURLGitLab("mendersoftware", "saas")
+	assert.NoError(t, err)
+	assert.Equal(t, "git@gitlab.com:Northern.tech/MenderSaaS/saas", url)
+
+	url, err = getRemoteURLGitLab("unknown", "saas")
+	assert.Error(t, err)
+	assert.Equal(t, "", url)
 }
