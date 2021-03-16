ad3b47002061fa9755ec371ad45bd6d3ea25185d
Ole Petter <ole.orhagen@northern.tech>
2021-03-16 11:07:20 +0100

    Add a utility function for batching git commands
    A lot of our code has 100+ lines of basically git boiler plate.
    
    This adds a utility package, for executing batch git commands.
    
    Changelog: None
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- entfork_sync.go
+++ entfork_sync.go
@@ -5,8 +5,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"io/ioutil"
-	"os"
 	"os/exec"
 	"regexp"
 	"strconv"
@@ -14,6 +12,7 @@ import (
 	"text/template"
 
 	"github.com/google/go-github/v28/github"
+	"github.com/mendersoftware/integration-test-runner/git"
 	"github.com/sirupsen/logrus"
 
 	clientgithub "github.com/mendersoftware/integration-test-runner/client/github"
@@ -129,89 +128,32 @@ func syncIfOSHasEnterpriseRepo(log *logrus.Entry, conf *config, gpr *github.Pull
 // and merges this with the OS equivalent of 'branchName'.
 func createPRBranchOnEnterprise(log *logrus.Entry, repo, branchName, PRNumber, PRBranchName string, conf *config) (merged bool, err error) {
 
-	tmpdir, err := ioutil.TempDir("", repo)
+	state, err := git.Commands(
+		git.Command("init", "."),
+		git.Command("remote", "add", "opensource",
+			getRemoteURLGitHub(conf.githubProtocol, githubOrganization, repo)),
+		git.Command("remote", "add", "enterprise",
+			getRemoteURLGitHub(conf.githubProtocol, githubOrganization, repo+"-enterprise")),
+		git.Command("remote", "add", githubBotName,
+			getRemoteURLGitHub(conf.githubProtocol, githubBotName, repo+"-enterprise")),
+		git.Command("config", "--add", "user.name", githubBotName),
+		git.Command("config", "--add", "user.email", "mender@northern.tech"),
+		git.Command("fetch", "opensource", branchName),
+		git.Command("git", "fetch", "enterprise", branchName+":"+PRBranchName),
+		git.Command("checkout", PRBranchName),
+	)
+	defer state.Cleanup()
 	if err != nil {
 		return false, err
-	}
-	defer os.RemoveAll(tmpdir)
-
-	gitcmd := exec.Command("git", "init", ".")
-	gitcmd.Dir = tmpdir
-	out, err := gitcmd.CombinedOutput()
-	if err != nil {
-		return false, fmt.Errorf("%v returned error: %s: %s", gitcmd.Args, out, err.Error())
-	}
-
-	repoURL := getRemoteURLGitHub(conf.githubProtocol, githubOrganization, repo)
-	gitcmd = exec.Command("git", "remote", "add", "opensource", repoURL)
-	gitcmd.Dir = tmpdir
-	out, err = gitcmd.CombinedOutput()
-	if err != nil {
-		return false, fmt.Errorf("%v returned error: %s: %s", gitcmd.Args, out, err.Error())
-	}
-
-	repoURL = getRemoteURLGitHub(conf.githubProtocol, githubOrganization, repo+"-enterprise")
-	gitcmd = exec.Command("git", "remote", "add", "enterprise", repoURL)
-	gitcmd.Dir = tmpdir
-	out, err = gitcmd.CombinedOutput()
-	if err != nil {
-		return false, fmt.Errorf("%v returned error: %s: %s", gitcmd.Args, out, err.Error())
-	}
-
-	repoURL = getRemoteURLGitHub(conf.githubProtocol, githubBotName, repo+"-enterprise")
-	gitcmd = exec.Command("git", "remote", "add", githubBotName, repoURL)
-	gitcmd.Dir = tmpdir
-	out, err = gitcmd.CombinedOutput()
-	if err != nil {
-		return false, fmt.Errorf("%v returned error: %s: %s", gitcmd.Args, out, err.Error())
-	}
-
-	// Set the local name to the github bot name, and the local
-	// email to 'mender@northern.tech'
-	gitcmd = exec.Command("git", "config", "--add", "user.name", githubBotName)
-	gitcmd.Dir = tmpdir
-	out, err = gitcmd.CombinedOutput()
-	if err != nil {
-		return false, fmt.Errorf("%v returned error: %s: %s", gitcmd.Args, out, err.Error())
-	}
-	gitcmd = exec.Command("git", "config", "--add", "user.email", "mender@northern.tech")
-	gitcmd.Dir = tmpdir
-	out, err = gitcmd.CombinedOutput()
-	if err != nil {
-		return false, fmt.Errorf("%v returned error: %s: %s", gitcmd.Args, out, err.Error())
-	}
-
-	// Fetch the branch which we are going to sync
-	gitcmd = exec.Command("git", "fetch", "opensource", branchName)
-	gitcmd.Dir = tmpdir
-	out, err = gitcmd.CombinedOutput()
-	if err != nil {
-		return false, fmt.Errorf("%v returned error: %s: %s", gitcmd.Args, out, err.Error())
-	}
-
-	// Fetch the Enterprise branch in which to merge into, and create the PR branch
-	gitcmd = exec.Command("git", "fetch", "enterprise", branchName+":"+PRBranchName)
-	gitcmd.Dir = tmpdir
-	out, err = gitcmd.CombinedOutput()
-	if err != nil {
-		return false, fmt.Errorf("%v returned error: %s: %s", gitcmd.Args, out, err.Error())
-	}
-
-	// Checkout the enterprise PR branch
-	gitcmd = exec.Command("git", "checkout", PRBranchName)
-	gitcmd.Dir = tmpdir
-	out, err = gitcmd.CombinedOutput()
-	if err != nil {
-		return false, fmt.Errorf("%v returned error: %s: %s", gitcmd.Args, out, err.Error())
 	}
 
 	// Merge the OS branch into the PR branch
 	mergeMsg := fmt.Sprintf("Merge OS base branch: (%s) including PR: (%s) into Enterprise: (%[1]s)",
 		branchName, PRNumber)
 	log.Debug("Trying to " + mergeMsg)
-	gitcmd = exec.Command("git", "merge", "-m", mergeMsg, "opensource/"+branchName)
-	gitcmd.Dir = tmpdir
-	out, err = gitcmd.CombinedOutput()
+	gitcmd := exec.Command("git", "merge", "-m", mergeMsg, "opensource/"+branchName)
+	gitcmd.Dir = state.Dir
+	out, err := gitcmd.CombinedOutput()
 	merged = true
 	if err != nil {
 		merged = false
@@ -229,7 +171,7 @@ func createPRBranchOnEnterprise(log *logrus.Entry, repo, branchName, PRNumber, P
 		// In case of a failed merge, reset PRBranchName to opensource/branchName
 		// and push this branch to enterprise
 		gitcmd = exec.Command("git", "reset", "--hard", "opensource/"+branchName)
-		gitcmd.Dir = tmpdir
+		gitcmd.Dir = state.Dir
 		out, err = gitcmd.CombinedOutput()
 		if err != nil {
 			return merged, fmt.Errorf("%v returned error: %s: %s", gitcmd.Args, out, err.Error())
@@ -238,7 +180,7 @@ func createPRBranchOnEnterprise(log *logrus.Entry, repo, branchName, PRNumber, P
 
 	// Push the branch to the bot's own fork
 	gitcmd = exec.Command("git", "push", "--set-upstream", githubBotName, PRBranchName)
-	gitcmd.Dir = tmpdir
+	gitcmd.Dir = state.Dir
 	out, err = gitcmd.CombinedOutput()
 	if err != nil {
 		return merged, fmt.Errorf("%v returned error: %s: %s", gitcmd.Args, out, err.Error())
